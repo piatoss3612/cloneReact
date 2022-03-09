5541aac499c9f0a662ba34273fc9afc66c792f15
Hyohwak Lee <crewe1746@naver.com>
2022-03-09 11:48:41 +0900

    virtual-keyboard 키보드 이벤트 적용

--- virtual-keyboard/src/js/keyboard.js
+++ virtual-keyboard/src/js/keyboard.js
@@ -1,6 +1,10 @@
 export class Keyboard {
+  #containerEl;
   #switchEl;
   #fontSelectEl;
+  #keyboardEl;
+  #inputGroupEl;
+  #inputEl;
 
   constructor() {
     this.#assignElement();
@@ -8,26 +12,50 @@ export class Keyboard {
   }
 
   #assignElement() {
-    this.#switchEl = document.getElementById("switch");
-    this.#fontSelectEl = document.getElementById("font");
+    this.#containerEl = document.getElementById("container");
+    this.#switchEl = this.#containerEl.querySelector("#switch");
+    this.#fontSelectEl = this.#containerEl.querySelector("#font");
+    this.#keyboardEl = this.#containerEl.querySelector("#keyboard");
+    this.#inputGroupEl = this.#containerEl.querySelector("#input-group");
+    this.#inputEl = this.#inputGroupEl.querySelector("#input");
   }
 
   #addEvent() {
-    this.#switchEl.addEventListener("change", (event) => {
-      //   if (event.target.checked) {
-      //     document.documentElement.setAttribute("theme", "dark-mode");
-      //   } else {
-      //     document.documentElement.setAttribute("theme", "");
-      //   }
-
-      document.documentElement.setAttribute(
-        "theme",
-        event.target.checked ? "dark-mode" : ""
-      );
-    });
-
-    this.#fontSelectEl.addEventListener("change", (event) => {
-      document.body.style.fontFamily = event.target.value;
-    });
+    this.#switchEl.addEventListener("change", this.#onChangeTheme);
+    this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
+    document.addEventListener("keydown", this.#onKeyUp);
+    document.addEventListener("keyup", this.#onKeyDown);
+    this.#inputEl.addEventListener("input", this.#onInput);
   }
+
+  #onChangeTheme = (event) => {
+    document.documentElement.setAttribute(
+      "theme",
+      event.target.checked ? "dark-mode" : ""
+    );
+  };
+
+  #onChangeFont = (event) => {
+    document.body.style.fontFamily = event.target.value;
+  };
+
+  #onKeyUp = (event) => {
+    this.#inputGroupEl.classList.toggle(
+      "error",
+      /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(event.key) || event.key === "Process"
+    );
+    this.#keyboardEl
+      .querySelector(`[data-code=${event.code}]`)
+      ?.classList.add("active");
+  };
+
+  #onKeyDown = (event) => {
+    this.#keyboardEl
+      .querySelector(`[data-code=${event.code}]`)
+      ?.classList.remove("active");
+  };
+
+  #onInput = (event) => {
+    event.target.value = event.target.value.replace(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/, "");
+  };
 }
