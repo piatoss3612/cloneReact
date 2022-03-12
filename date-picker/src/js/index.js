e8ecc2d6ee017d36aa4af134a17f31c247932fec
Hyohwak Lee <crewe1746@naver.com>
2022-03-12 16:28:41 +0900

    date-picker 날짜 선택 기능 추가

--- date-picker/src/js/index.js
+++ date-picker/src/js/index.js
@@ -38,8 +38,10 @@ class DatePicker {
   calendarDatesEl;
 
   constructor() {
-    this.assignElement();
     this.initCalendarDate();
+    this.initSelectedDate();
+    this.assignElement();
+    this.setDateInput();
     this.addEvent();
   }
 
@@ -67,13 +69,26 @@ class DatePicker {
     };
   };
 
+  initSelectedDate = () => {
+    this.selectedDate = { ...this.#calendarDate };
+  };
+
+  setDateInput = () => {
+    this.dateInputEl.textContent = this.formatDate(this.selectedDate.data);
+    this.dateInputEl.dataset.value = this.selectedDate.data;
+  };
+
   addEvent = () => {
     this.dateInputEl.addEventListener('click', this.toggleCalendar);
     this.nextBtnEl.addEventListener('click', this.moveToNextMonth);
     this.prevBtnEl.addEventListener('click', this.moveToPrevMonth);
+    this.calendarDatesEl.addEventListener('click', this.onClickSelectDate);
   };
 
   toggleCalendar = () => {
+    if (this.calendarEl.classList.contains('active')) {
+      this.#calendarDate = { ...this.selectedDate };
+    }
     this.calendarEl.classList.toggle('active');
     this.updateMonth();
     this.updateDates();
@@ -108,6 +123,7 @@ class DatePicker {
     this.colorSaturday();
     this.colorSunday();
     this.markToday();
+    this.markSelectedDate();
   };
 
   colorSaturday = () => {
@@ -156,6 +172,17 @@ class DatePicker {
     }
   };
 
+  markSelectedDate = () => {
+    if (
+      this.selectedDate.year === this.#calendarDate.year &&
+      this.selectedDate.month === this.#calendarDate.month
+    ) {
+      this.calendarDatesEl
+        .querySelector(`[data-date='${this.selectedDate.date}']`)
+        .classList.add('selected');
+    }
+  };
+
   moveToNextMonth = () => {
     this.#calendarDate.month++;
     if (this.#calendarDate.month > 11) {
@@ -174,6 +201,41 @@ class DatePicker {
     }
     this.updateMonth();
     this.updateDates();
+  };
+
+  onClickSelectDate = event => {
+    const eventTarget = event.target;
+    if (eventTarget.dataset.date) {
+      this.calendarDatesEl
+        .querySelector('.selected')
+        ?.classList.remove('selected');
+      eventTarget.classList.add('selected');
+      this.selectedDate = {
+        data: new Date(
+          this.#calendarDate.year,
+          this.#calendarDate.month,
+          eventTarget.dataset.date,
+        ),
+        year: this.#calendarDate.year,
+        month: this.#calendarDate.month,
+        date: eventTarget.dataset.date,
+      };
+      this.setDateInput();
+      this.calendarEl.classList.remove('active');
+    }
+  };
+
+  formatDate = dateData => {
+    let date = dateData.getDate();
+    if (date < 10) {
+      date = `0${date}`;
+    }
+    let month = dateData.getMonth() + 1;
+    if (month < 10) {
+      month = `0${month}`;
+    }
+    let year = dateData.getFullYear();
+    return `${year}/${month}/${date}`;
   };
 }
 
