440fd254c0dadfb6bba2fdc20c8743a8bf103e77
Hyohwak Lee <crewe1746@naver.com>
2022-04-26 21:48:06 +0900

    kakao-map: 카카오 지도 응용하기

--- practice/kakao-map/src/App.tsx
+++ practice/kakao-map/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 declare global {
   interface Window {
@@ -8,6 +8,8 @@ declare global {
 
 function App() {
   const mapRef = useRef<HTMLDivElement>(null);
+  const map = useRef<any>(null);
+  const [markerList, setMarkerList] = useState<any[]>([]);
 
   useEffect(() => {
     const container = mapRef.current;
@@ -25,7 +27,22 @@ function App() {
           center: new window.kakao.maps.LatLng(33.450701, 126.570667),
           level: 3,
         };
-        new window.kakao.maps.Map(container, options);
+        map.current = new window.kakao.maps.Map(container, options);
+
+        window.kakao.maps.event.addListener(
+          map.current,
+          "rightclick",
+          (mouseEvent: any) => {
+            var latlng = mouseEvent.latLng;
+            const title = prompt("마커의 제목을 입력하세요:");
+            var marker = new window.kakao.maps.Marker({
+              map: map.current,
+              position: latlng,
+              title,
+            });
+            setMarkerList((prev) => [...prev, marker]);
+          }
+        );
       });
     };
 
@@ -35,6 +52,54 @@ function App() {
   return (
     <>
       <div ref={mapRef} id="map" style={{ width: 300, height: 300 }}></div>
+      <div>
+        <button
+          onClick={() => {
+            map.current.setCenter(
+              new window.kakao.maps.LatLng(37.5665, 126.978)
+            );
+          }}
+        >
+          서울
+        </button>
+        <button
+          onClick={() => {
+            map.current.setCenter(
+              new window.kakao.maps.LatLng(35.1796, 129.0756)
+            );
+          }}
+        >
+          부산
+        </button>
+        <input
+          type="range"
+          min="1"
+          max="20"
+          onChange={(event) => {
+            map.current.setLevel(event.target.value, { animate: true });
+          }}
+        />
+        <button
+          onClick={() => {
+            map.current.setMapTypeId(window.kakao.maps.MapTypeId.HYBRID);
+          }}
+        >
+          지도 타입 변경
+        </button>
+      </div>
+      <ul>
+        {markerList.map((marker) => (
+          <li
+            key={marker.getTitle()}
+            onClick={() => {
+              marker.setMap(null);
+              setMarkerList(markerList.filter((value) => value !== marker));
+            }}
+          >
+            {marker.getTitle()}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
