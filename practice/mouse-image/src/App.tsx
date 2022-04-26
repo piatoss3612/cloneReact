b72a44302156992b74d6ca0c9be94d333301b5a3
Hyohwak Lee <crewe1746@naver.com>
2022-04-26 18:06:31 +0900

    mouse-image 기본 디스플레이 작성

--- practice/mouse-image/src/App.tsx
+++ practice/mouse-image/src/App.tsx
@@ -1,5 +1,36 @@
+import { useState } from "react";
+import Button from "./components/Button";
+
+type Cursor = "blue_cursor" | "cursor1" | "cursor2" | "cursor3";
+
+const cursors: Cursor[] = ["blue_cursor", "cursor1", "cursor2", "cursor3"];
+
 function App() {
-  return <>App</>;
+  const [selectedCursor, setSelectedCursor] = useState<Cursor>("blue_cursor");
+  return (
+    <>
+      <div style={{ fontSize: "24px" }}>
+        버튼을 눌러서 마우스 커서를 바꿔보세요.
+      </div>
+      <div
+        style={{
+          marginTop: "16px",
+          display: "flex",
+          gap: "20px",
+          flexWrap: "wrap",
+        }}
+      >
+        {cursors.map((cursor) => (
+          <Button
+            key={cursor}
+            name={cursor}
+            onClick={setSelectedCursor.bind(null, cursor)}
+            selected={selectedCursor === cursor}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ practice/mouse-image/src/components/Button.tsx
@@ -0,0 +1,29 @@
+interface ButtopProps {
+  selected?: boolean;
+  name: string;
+  onClick: () => void;
+}
+
+const Button = ({ selected, name, onClick }: ButtopProps) => {
+  return (
+    <div
+      style={{
+        width: "100px",
+        height: "100px",
+        border: `solid ${selected ? 6 : 1}px black`,
+        boxSizing: "border-box",
+        padding: "16px",
+        cursor: "pointer",
+      }}
+      onClick={onClick}
+    >
+      <img
+        src={`/images/${name}.png`}
+        style={{ width: "100%", height: "100%" }}
+        alt={name}
+      />
+    </div>
+  );
+};
+
+export default Button;
