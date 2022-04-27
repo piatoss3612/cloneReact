2911f5f1c6a948d27489d7019fe401fa859ff5ea
Hyohwak Lee <crewe1746@naver.com>
2022-04-27 22:30:20 +0900

    music-player play list에 음악 재생 시간 표시

--- practice/music-player/src/components/PlayList/PlayListItem.jsx
+++ practice/music-player/src/components/PlayList/PlayListItem.jsx
@@ -1,8 +1,35 @@
 import classNames from "classnames";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
+const getDuration = (src) => {
+  return new Promise((resolve) => {
+    const audio = new Audio();
+    audio.onloadedmetadata = () => {
+      const tempMin = parseInt(audio.duration / 60, 10);
+      const tempSec = parseInt(audio.duration % 60);
+      const minutes = tempMin > 9 ? tempMin : `0${tempMin}`;
+      const seconds = tempSec > 9 ? tempSec : `0${tempSec}`;
+      console.log(minutes, seconds);
+      resolve(`${minutes}:${seconds}`);
+    };
+    audio.src = src;
+  });
+};
+
 const PlayListItem = ({ item, index }) => {
+  const [duration, setDuration] = useState("00:00");
   const currentIndex = useSelector((state) => state.currentIndex);
+
+  useEffect(() => {
+    const getTime = async () => {
+      const itemDuration = await getDuration(item.src);
+      setDuration(itemDuration);
+    };
+
+    getTime();
+  }, [item.src]);
+
   return (
     <>
       <div className={classNames("row", { playing: currentIndex === index })}>
@@ -14,7 +41,7 @@ const PlayListItem = ({ item, index }) => {
           playing: currentIndex === index,
         })}
       >
-        00:00
+        {duration}
       </span>
     </>
   );
