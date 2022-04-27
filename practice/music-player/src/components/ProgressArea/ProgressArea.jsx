50edc165e1288929de084c982514bc72d3d51664
Hyohwak Lee <crewe1746@naver.com>
2022-04-27 18:28:37 +0900

    music-player progress bar 진행도 표시 및 클릭 이벤트 구현

--- practice/music-player/src/components/ProgressArea/ProgressArea.jsx
+++ practice/music-player/src/components/ProgressArea/ProgressArea.jsx
@@ -1,4 +1,4 @@
-import { useRef, forwardRef, useImperativeHandle } from "react";
+import { useRef, forwardRef, useImperativeHandle, useState } from "react";
 import { useDispatch } from "react-redux";
 import { playMusic, stopMusic } from "../../store/musicPlayerReducer";
 import "./ProgressArea.scss";
@@ -7,6 +7,9 @@ import music1 from "../../music/music-1.mp3";
 
 const ProgressArea = forwardRef((props, ref) => {
   const audio = useRef();
+  const progressBar = useRef();
+  const [currentTime, setCurrentTime] = useState("00:00");
+  const [duration, setDuration] = useState("00:00");
   const dispatch = useDispatch();
 
   useImperativeHandle(ref, () => ({
@@ -26,20 +29,48 @@ const ProgressArea = forwardRef((props, ref) => {
     dispatch(stopMusic());
   };
 
+  const getTime = (time) => {
+    const currentMin = parseInt(time / 60, 10);
+    const currentSec = parseInt(time % 60);
+    const minutes = currentMin > 9 ? currentMin : `0${currentMin}`;
+    const seconds = currentSec > 9 ? currentSec : `0${currentSec}`;
+    return `${minutes}:${seconds}`;
+  };
+
+  const onTimeUpdate = (event) => {
+    if (event.target.readyState === 0) return;
+    const currentTime = event.target.currentTime;
+    const duration = event.target.duration;
+    const progressBarWidth = (currentTime / duration) * 100;
+    progressBar.current.style.width = `${progressBarWidth}%`;
+    setCurrentTime(getTime(currentTime));
+    setDuration(getTime(duration));
+  };
+
+  const onClickProgress = (event) => {
+    const progressBarWidth = event.currentTarget.clientWidth;
+    const offsetX = event.nativeEvent.offsetX;
+    const duration = audio.current.duration;
+
+    console.log(progressBarWidth, offsetX, duration);
+    audio.current.currentTime = (offsetX / progressBarWidth) * duration;
+  };
+
   return (
-    <div className="progress-area">
-      <div className="progress-bar">
+    <div className="progress-area" onMouseDown={onClickProgress}>
+      <div className="progress-bar" ref={progressBar}>
         <audio
           autoPlay
           ref={audio}
           onPlay={onPlay}
           onPause={onPause}
+          onTimeUpdate={onTimeUpdate}
           src={music1}
         ></audio>
       </div>
       <div className="music-timer">
-        <span>00:00</span>
-        <span>00:00</span>
+        <span>{currentTime}</span>
+        <span>{duration}</span>
       </div>
     </div>
   );
